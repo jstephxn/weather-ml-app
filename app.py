4fc88edf22ef01a6f9dbda1894418a5cee68c4d5
Christopher Scott <cjscott05@icloud.com>
2025-11-29 20:47:51 +0000

    fixed a syntax error in the weather_classes list, a bug in classify_weather method, converted all form data to floats, and added get method

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
- GNU nano 7.2                                     app.py
 from flask import Flask, request, render_template
 import pickle
 import numpy as np
@@ -6,7 +5,7 @@ import time
 
 app = Flask(__name__)
 
-weather_classes = ['clear', 'cloudy', 'drizzly', 'foggy', 'hazey', 'misty', 'rainy', 'smokey', 'thunde>
+weather_classes = ['clear', 'cloudy', 'drizzly', 'foggy', 'hazey', 'misty', 'rainy', 'smokey', 'thunderstorm']
 
 def load_model(model_path = 'model/model.pkl'):
         return pickle.load(open(model_path, 'rb'))
@@ -16,7 +15,7 @@ def classify_weather(features):
         start = time.time()
         prediction_index = model.predict(features)[0]
         latency = round((time.time() - start) * 1000, 2) #we are here
-        prediction = weather_classes[1]
+        prediction = weather_classes[prediction_index]
 
         return prediction, latency
 
@@ -26,11 +25,11 @@ def home():
         if request.method == 'POST':
                 try:
                         # Extract floats from form data
-                        temperature = request.form['temperature']
-                        pressure = request.form['pressure']
-                        humidity = request.form['humidity']
-                        wind_speed = request.form['wind_speed']
-                        wind_deg = request.form['wind_deg']
+                        temperature = float(request.form['temperature'])
+                        pressure = float(request.form['pressure'])
+                        humidity = float(request.form['humidity'])
+                        wind_speed = float(request.form['wind_speed'])
+                        wind_deg = float(request.form['wind_deg'])
                         rain_1h = float(request.form.get('rain_1h', 0) or 0)
                         rain_3h = float(request.form.get('rain_3h', 0) or 0)
                         snow = float(request.form.get('snow', 0) or 0)
@@ -51,6 +50,10 @@ def home():
                 except Exception as e:
                         error_msg = f"Error processing input: {e}"
                         return render_template('form.html', error=error_msg)
+                
         # GET method: show the input form
- GNU nano 7.2                                     app.py
+        return render_template('form.html')
 
+if __name__ == '__main__':
+    # Run the Flask app
+    app.run(host='0.0.0.0', port=5001)
